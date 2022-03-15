2a36bd8f333b9e0bd868539e7585d29d826540c1
edwardalex <olooedwards69@gmail.com>
2022-03-15 10:20:52 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - thi program prints every minute
+ *
+ * @n: first value
+ * @m: second value
+ *
+ * Return: minutes
+ */
+int add(int n, int m)
+{
+	int result = n + m;
+
+	return (result);
+}
