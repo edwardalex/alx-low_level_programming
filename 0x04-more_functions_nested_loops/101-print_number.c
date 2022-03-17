58b1bbeabb2cb38b55f9a4a5f9ece6781228f98e
edwardalex <olooedwards69@gmail.com>
2022-03-17 03:11:28 -0700

    Numbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,21 +1,48 @@
 #include "main.h"
 
+void print_prev_digits(int n);
+
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
+ * print_number - prints a number using _putchar
+ *
+ * @n: number to print
+ * Return: void
  */
 void print_number(int n)
 {
-	unsigned int num = n;
-
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
 	}
+	if (n > 9 || n < -9)
+		print_prev_digits(n);
+	if (n < 0)
+		_putchar(((n % 10) * -1) + '0');
+	else
+		_putchar((n % 10) + '0');
+}
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
+/**
+ * print_prev_digits - recursive function to print all previous digits in num
+ *
+ * @n: number to print previous digits of
+ * Return: void
+ */
+void print_prev_digits(int n)
+{
+	if (((n / 10) < -9) || ((n / 10) > 9))
+	{
+		print_prev_digits(n / 10);
+		if (n < 0)
+			_putchar((((n / 10) % 10) * -1) + '0');
+		else
+			_putchar(((n / 10) % 10) + '0');
+	}
+	else
+	{
+		if (n < 0)
+			_putchar(((n / 10) * -1) + '0');
+		else
+			_putchar((n / 10) + '0');
+	}
 }
