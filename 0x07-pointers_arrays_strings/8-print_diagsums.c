e8e78149f690db4f4780a339c2c45565796b9ac2
edwardalex <olooedwards69@gmail.com>
2022-03-28 04:46:12 -0700

    Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,25 +1,20 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * print_diagsums - this function gets the legth in bytes
- *
- * @a: string
- * @size: string
- *
+ * print_diagsums -  prints sums
+ * @a: a pointer
+ * @size: size
  */
 void print_diagsums(int *a, int size)
 {
-	int i = 0;
-	int x = 0;
-	int x1 = 0;
+	int i, sum1 = 0, sum2 = 0;
 
-	for (i = 0; i <= (size * size) - 1; i += size + 1)
+	for (i = 0; i < size; i++)
 	{
-		x += a[i];
+		sum1 += *(a + (size * i + i));
+		sum2 += *(a + (size * i + size - 1 - i));
 	}
-	for (i = size * (size - 1); i >= 0; i -= size - 1)
-	{
-		x1 += a[i];
-	}
-	printf("%d, %d\n", x, x1);
+	printf("%d, ", sum1);
+	printf("%d\n", sum2);
 }
