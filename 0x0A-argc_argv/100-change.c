59c9ba111dfe517ba5b94292ef47e382924fe4cb
edwardalex <olooedwards69@gmail.com>
2022-04-01 06:09:38 -0700

    update

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,50 +1,55 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
- * main - this program prints its name
- *
- * @argc: number of arguments
- * @argv: string of arguments
- *
+ * main - prints the minimum number of coins
+ * to make change for an amount of money
+ * @argc: n args
+ * @argv: arr args
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int coins[] = { 25, 10, 5, 2, 1 };
-	int i;
-	int x = atoi(argv[1]);
-	int value = 0;
+	int value, c;
 
-	if (x <= 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-	if (x > 0 && argc == 2)
-	{
-		for (i = 0; x > 0; i++)
-		{
-			if (coins[i] <= x)
-			{
-				x = x - coins[i];
-				i--;
-				value++;
-			}
-			else if (coins[i] == x)
-			{
-				printf("1\n");
-				return (0);
-			}
-		}
-		printf("%d\n", value);
-		return (0);
-	}
-	else
+	c = 0;
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
+	value = atoi(argv[1]);
+	if (value < 0)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	if (value % 25 >= 0)
+	{
+		c += value / 25;
+		value = value % 25;
+	}
+	if (value % 10 >= 0)
+	{
+		c += value / 10;
+		value = value % 10;
+	}
+	if (value % 5 >= 0)
+	{
+		c += value / 5;
+		value = value % 5;
+	}
+	if (value % 2 >= 0)
+	{
+		c += value / 2;
+		value = value % 2;
+	}
+	if (value % 1 >= 0)
+	{
+		c += value / 1;
+	}
+	printf("%d\n", c);
 	return (0);
 }
